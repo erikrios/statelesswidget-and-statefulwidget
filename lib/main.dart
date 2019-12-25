d67e55cf536865de89c39daa1a2e9985261a779c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2019-12-25 20:46:38 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Demo Flutter',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: HomePage(),
+      );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomeState();
+}
+
+class HomeState extends State<HomePage> {
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrementCounter() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Demo StatefulWidget'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Nilai counter: $counter',
+                style: TextStyle(fontSize: 30.0),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('<<'),
+                    onPressed: decrementCounter,
+                  ),
+                  Container(
+                    width: 10.0,
+                  ),
+                  RaisedButton(
+                    child: Text('>>'),
+                    onPressed: incrementCounter,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+}
